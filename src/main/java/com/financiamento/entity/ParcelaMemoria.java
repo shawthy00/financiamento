86fd7177de313768ee1a62848b9851b698b660b8
shawthy <brito.danilohenriques@gmail.com>
2026-05-19 20:57:58 -0300

    entities Simulacao e ParcelaMemoria

--- src/main/java/com/financiamento/entity/ParcelaMemoria.java
+++ src/main/java/com/financiamento/entity/ParcelaMemoria.java
@@ -1,4 +1,32 @@
 package com.financiamento.entity;
 
-public class ParcelaMemoria {
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "parcelas_memoria")
+public class ParcelaMemoria extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    //todos elementos das tuplas do SGBD devem ser não nulos
+    @Column(nullable = false)
+    public Integer mes;
+
+    @Column(nullable = false)
+    public BigDecimal saldoInicial;
+
+    @Column(nullable = false)
+    public BigDecimal juro;
+
+    @Column(nullable = false)
+    public BigDecimal saldoFinal;
+
+    @ManyToOne
+    @JoinColumn (name = "simulacao_id", nullable = false)   // FK para id do objeto Simulacoes
+    public Simulacao simulacao;
 }
--- src/main/java/com/financiamento/entity/Simulacao.java
+++ src/main/java/com/financiamento/entity/Simulacao.java
@@ -1,4 +1,40 @@
 package com.financiamento.entity;
 
-public class Simulacao {
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity // criando o banco de dados
+@Table(name = "simulacoes")
+public class Simulacao extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(nullable = false)
+    public BigDecimal valorInicial;
+
+    @Column(nullable = false)
+    public BigDecimal taxaJurosMensal;
+
+    @Column(nullable = false)
+    public Integer prazoMeses;
+
+    @Column(nullable = false)
+    public BigDecimal valorTotalFinal;
+
+    @Column
+    public BigDecimal valorTotalJuros;
+
+    @Column
+    public LocalDateTime criadoEm = LocalDateTime.now(); //se relaciona com o dia que foi feita, para nao ter juro de acerto
+
+    @OneToMany(mappedBy = "simulacao", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    public List<ParcelaMemoria> memoriaCalculo;
+
+
 }
