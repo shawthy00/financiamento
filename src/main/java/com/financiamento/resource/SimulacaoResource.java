f3d19c90edcb9917b11ea305571a5e9d54ace73b
shawthy <brito.danilohenriques@gmail.com>
2026-05-20 20:12:05 -0300

    resource

---
+++ src/main/java/com/financiamento/resource/SimulacaoResource.java
@@ -0,0 +1,37 @@
+package com.financiamento.resource;
+
+import com.financiamento.service.SimulacaoService;
+import com.financiamento.dto.SimulacaoResponse;
+import com.financiamento.dto.SimulacaoRequest;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/simulacoes")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class SimulacaoResource {
+
+    @Inject
+    SimulacaoService service;
+
+    @POST
+    public Response simular(SimulacaoRequest request) {
+        SimulacaoResponse response = service.simular(request);
+
+        return Response.status(201).entity(response).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response buscaPorId(@PathParam("id") Long id) {
+        SimulacaoResponse response = service.buscaPorId(id);
+
+        if (response == null) {
+            return Response.status(404).build();
+        }
+
+        return Response.ok(response).build();
+    }
+}
